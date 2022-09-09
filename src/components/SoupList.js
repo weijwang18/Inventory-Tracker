29e1e4ebe8da1fea3db712d6f8437cb551d4ba61
Winnie Wang <weijwang18@gmail.com>
2022-09-09 15:58:34 -0700

    update SoupList.js

--- src/components/SoupList.js
+++ src/components/SoupList.js
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Soup from "./Soup";
 
-function SoupList(){
+function SoupList(props){
   return (
     <React.Fragment>
-        
+      {props.soupList.map((soup, index) =>
+        <Soup name = {soup.name}
+        price = {soup.price}
+        quantity = {soup.quantity}
+        key = {index} />
+      )}
     </React.Fragment>
   );
+}
+
+SoupList.prototypes = {
+  soupList: PropTypes.array
 }
 
 export default SoupList;
